abf8f877d9c2d65d5ea01a3ae1219393fe8bd18e
DiegoLaurentin <diego.laurentin.ac@gmail.com>
2023-09-25 21:06:49 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="h-screen bg-black relative">
+      {/* Contenedor de la imagen */}
+      <div className="img-container">
+        <img src="./src/assets/laurentin-positivo.png" alt="Logo" />
+      </div>
+
+      {/* Contenido principal */}
+      <div className="flex items-center justify-center h-full">
+        <div className='w-max'>
+
+          <h1 className="text-white text-6xl font-medium animate-typing overflow-hidden whitespace-nowrap border-r-4 border-r-white pr-5">Próximamente...</h1>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,58 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      keyframes: {
+        typing: {
+          "0%": {
+            width: "0%",
+            visibility: "hidden"
+          },
+          "100%": {
+            width: "100%"
+          }  
+        },
+        blink: {
+          "50%": {
+            borderColor: "transparent"
+          },
+          "100%": {
+            borderColor: "white"
+          }  
+        }
+      },
+      animation: {
+        typing: "typing 5s steps(20) infinite alternate, blink .7s infinite"
+      },
+      backgroundColor: {
+        'black': '#202023',
+      },
+      textColor: {
+        'white': '#F7F6F2'
+      },
+      fontSize: {
+        'xs': '0.75rem',
+        'sm': '0.875rem',
+        'base': '1rem',
+        'lg': '1.125rem',
+        'xl': '1.25rem',
+        '2xl': '1.5rem',
+        '3xl': '1.875rem',
+        '4xl': '2.25rem',
+        '5xl': '3rem',
+        '6xl': '4rem',
+      },
+      fontFamily: {
+        'normal': ['AtypDisplay-Regular', 'sans'],
+        'bold': ['AtypDisplay-Bold', 'sans'],
+        'light': ['AtypDisplay-Light', 'sans'],
+      },
+    },
+  },
+  plugins: [],
+}
+
