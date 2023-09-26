49bd53d410f56dc16cf9e65e34bd63cc04f18c09
DiegoLaurentin <diego.laurentin.ac@gmail.com>
2023-09-25 23:06:06 -0400

    feat/responsive-design

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,20 @@
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  // Actualiza el estado de windowWidth cuando cambia el ancho de la ventana
+  useEffect(() => {
+    function handleResize() {
+      setWindowWidth(window.innerWidth);
+    }
+
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   return (
     <div className="h-screen bg-black relative">
@@ -8,13 +22,12 @@ function App() {
         <img src="laurentin-positivo.png" alt="Logo" />
       </div>
       <div className="flex items-center justify-center h-full">
-        <div className='w-max'>
-          <h1 className="text-white text-6xl font-medium animate-typing overflow-hidden whitespace-nowrap border-r-4 border-r-white pr-5">Próximamente...</h1>
+        <div className={`w-max ${windowWidth <= 768 ? 'text-3xl' : 'text-6xl'}`}>
+          <h1 className="text-white font-medium animate-typing overflow-hidden whitespace-nowrap border-r-4 border-r-white pr-5">Próximamente...</h1>
         </div>
       </div>
     </div>
-  )
-  
+  );
 }
 
-export default App
+export default App;
